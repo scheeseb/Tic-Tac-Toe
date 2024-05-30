d3bac84e1362b9fdaa8ba00889adb12a3c8067cd
scheeseb <samuel.cheeseboro@gmail.com>
2024-05-30 02:36:06 -0500

    Tried to add checkForWin to the gameBoard as a property.

--- script.js
+++ script.js
@@ -16,13 +16,77 @@ const gameBoard = (function createGameBoard(size) {
         }
         board.push(boardRow)
     }
-    return { board }
+
+    const checkForWin = function checkForWin(marker) {
+        const gameBoardLength = gameBoard.board.length;
+    
+        // check all horizontal matches
+        (function checkX() {
+            gameBoard.board.forEach(function (array) {
+                let hitStatus = 0;
+    
+                array.forEach(function (mark) {
+                    if (mark === marker) {
+                        hitStatus++
+                    }
+                })
+    
+                if (hitStatus === array.length) {
+                    console.log("True X axis")
+                    return true;
+                }
+            })
+        })();
+        // 
+        // Check Vertical for matches
+        (function checkY() {
+    
+            for (i = 0; i < gameBoardLength; i++) {
+                let hitStatus = 0;
+                for (j = 0; j < gameBoardLength; j++) {
+                    if (gameBoard.board[j][i] === marker) {
+                        hitStatus++
+                    }
+                    if (hitStatus === gameBoardLength) {
+                        return true;
+                    }
+                }
+            }
+        })();
+        //
+        // Check for diagonal matches
+        (function checkDiag() {
+            let hitStatusT = 0;
+            for (i = 0; i < gameBoardLength; i++) {
+                if (gameBoard.board[i][i] === marker) {
+                    hitStatusT++;
+                }
+            }
+            if (hitStatusT === gameBoardLength) {
+                return true;
+            }
+            let hitStatusB = 0;
+            // Bottom Left to Top right
+            for (i = 0; i < gameBoardLength; i++) {
+                if (gameBoard.board[gameBoardLength - i - 1][i] === marker) {
+                    hitStatusB++
+                }
+            }
+            if (hitStatusB === gameBoardLength) {
+                return true;
+            }else {return false}
+        })();
+    }
+
+    return { board, checkForWin }
 
 })(3)
 console.log(gameBoard.board)
 
 
 function checkForWin(marker) {
+    const gameBoardLength = gameBoard.board.length;
+
     // check all horizontal matches
     (function checkX() {
         gameBoard.board.forEach(function (array) {
@@ -44,14 +108,17 @@ function checkForWin(marker) {
     // Check Vertical for matches
     (function checkY() {
 
-        for (i = 0; i < gameBoard.board.length; i++) {
+        for (i = 0; i < gameBoardLength; i++) {
             let hitStatus = 0;
-            for (j = 0; j < gameBoard.board.length; j++) {
+            for (j = 0; j < gameBoardLength; j++) {
                 if (gameBoard.board[j][i] === marker) {
                     hitStatus++
                 }
-                if (hitStatus === gameBoard.board.length) {
-                    console.log('True Y axis')
+                if (hitStatus === gameBoardLength) {
+                    console.log('True Y axis');
+                    return true;
+                } else if(i === gameBoardLength){
+                    return false
                 }
             }
         }
@@ -59,7 +126,25 @@ function checkForWin(marker) {
     //
     // Check for diagonal matches
     (function checkDiag() {
-
+        let hitStatusT = 0;
+        for (i = 0; i < gameBoardLength; i++) {
+            if (gameBoard.board[i][i] === marker) {
+                hitStatusT++;
+            }
+        }
+        if (hitStatusT === gameBoardLength) {
+            return true;
+        }
+        let hitStatusB = 0;
+        // Bottom Left to Top right
+        for (i = 0; i < gameBoardLength; i++) {
+            if (gameBoard.board[gameBoardLength - i - 1][i] === marker) {
+                hitStatusB++
+            }
+        }
+        if (hitStatusB === gameBoardLength) {
+            return true;
+        }else {return false}
     })();
 }
 
